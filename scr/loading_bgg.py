4f62e197181e22ac69d026080d9055853e9a3c35
Nikita-Fedorov <fedorov.nvad@gmail.com>
2023-04-13 20:55:04 +0300

    [+] add loader BGG user data from BGG API

--- scr/loading_bgg.py
+++ scr/loading_bgg.py
@@ -1,13 +1,15 @@
+import time
 import requests as re
 from bs4 import BeautifulSoup
 
 
-def get_api_bgg_game_data(unique_ids: list, save_path: str) -> list:
+def get_api_bgg_game_data(unique_ids: list, save_path: str):
     """
-    This function retrieves aggregated game information via the boardgamegeek.com API
+    This function retrieves user ratings information via the boardgamegeek.com API
 
     ==========
-	Parameters:
+	Parameters
+	==========
     unique_ids: list
         - list of unique boardgames ids (it is best to feed a list of no more than 50 id)
     save_path: str
@@ -39,9 +41,38 @@ def get_api_bgg_game_data(unique_ids: list, save_path: str) -> list:
     api_boardgame = 'https://api.geekdo.com/xmlapi/boardgame/'
     api_params = '?stats=1&pricehistory=1&marketplace=1&comments=1'
     r = re.get(api_boardgame + url_id + api_params)
-    soup = BeautifulSoup(r.text, 'xml')
+    soup = BeautifulSoup(r.text, features='xml')
     list_bg = soup.find_all('boardgame')
     for bg in list_bg:
         boardgame_id = bg.get('objectid')
         with open(save_path + '/' + str(boardgame_id) + '.xml', 'w', encoding='utf-8') as f:
             f.write(str(bg))
+
+
+def get_api_user_ratings_data(nickname, save_path=None, sleep=False):
+    """
+    This function takes the boardgamegeek.com username as input
+    and returns a pandas DataFrame with all of the user's scores
+    ==========
+	Parameters
+    ==========
+    nickname: str
+        - user nickname for getting ratings
+    sleep: boolean
+        - if True we use 15sec lag
+    save_path: str
+        - path for saving scraped boardgame's files
+    """
+    url_coll_main = 'https://api.geekdo.com/xmlapi/collection/'
+    params = '?rated=1'
+
+    if sleep:
+        r = re.get(url_coll_main + nickname + params)
+        time.sleep(14.66)
+    time.sleep(0.33)
+    r = re.get(url_coll_main + nickname + params)
+
+    if save_path:
+        soup = BeautifulSoup(r.text, features="xml")
+        with open(save_path + '/' + str(nickname) + '.xml', 'w', encoding='utf-8') as f:
+            f.write(str(soup))
