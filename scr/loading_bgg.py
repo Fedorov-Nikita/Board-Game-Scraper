7bbbcf1a9d84ae733294767bd3996f9797f0dcc4
Nikita-Fedorov <fedorov.nvad@gmail.com>
2023-04-14 16:46:47 +0300

    [+] add function for loading user ratings data

--- scr/loading_bgg.py
+++ scr/loading_bgg.py
@@ -1,23 +1,19 @@
 import time
 import requests as re
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 import sqlite3
 
 
 def get_api_bgg_game_data(unique_ids: list, save_path: str):
     """
     This function retrieves user ratings information via the boardgamegeek.com API
-
-    ==========
-	Parameters
-	==========
-    unique_ids: list
-        - list of unique boardgames ids (it is best to feed a list of no more than 50 id)
-    save_path: str
-        - path for saving scraped boardgame's files
-
-    ----------
-    Using API BoardGameGeek:
+    ====================
+    :param unique_ids: list - list of unique boardgames ids (it is best to feed a list of no more than 50 id)
+    :param save_path: str - path for saving scraped boardgame's files
+    :return: None
+    ====================
+    BoardGameGeek API:
     https://api.geekdo.com/xmlapi/boardgame/37111?stats=1&pricehistory=1&marketplace=1&comments=1
 
     base - https://api.geekdo.com/xmlapi/boardgame
@@ -30,7 +26,6 @@ def get_api_bgg_game_data(unique_ids: list, save_path: str):
     to: Set the end date to include historical data (format: YYYY-MM-DD, absent by default )
     pricehistory: retrieve the marketplace history for this item (set it to 1, absent by default)
     marketplace: retrieve the current marketplace listings (set it to 1, absent by default)
-
     """
     url_id = ''
     for i in range(len(unique_ids)):
@@ -50,32 +45,24 @@ def get_api_bgg_game_data(unique_ids: list, save_path: str):
             f.write(str(bg))
 
 
-def get_api_user_ratings_data(nickname, save_path=None, sleep=False):
+def get_api_user_ratings_data(nickname, PATH_TO_SAVE=None):
     """
     This function takes the boardgamegeek.com username as input
     and returns a pandas DataFrame with all of the user's scores
-    ==========
-	Parameters
-    ==========
-    nickname: str
-        - user nickname for getting ratings
-    sleep: boolean
-        - if True we use 15sec lag
-    save_path: str
-        - path for saving scraped boardgame's files
+
+    :param nickname: str - user nickname for getting ratings
+    :param PATH_TO_SAVE: str - path for saving scraped user ratings files
+    :return: None
     """
     url_coll_main = 'https://api.geekdo.com/xmlapi/collection/'
     params = '?rated=1'
 
-    if sleep:
-        r = re.get(url_coll_main + nickname + params)
-        time.sleep(14.66)
     time.sleep(0.33)
     r = re.get(url_coll_main + nickname + params)
 
-    if save_path:
+    if PATH_TO_SAVE:
         soup = BeautifulSoup(r.text, features="xml")
-        with open(save_path + '/' + str(nickname) + '.xml', 'w', encoding='utf-8') as f:
+        with open(PATH_TO_SAVE + '/' + str(nickname) + '.xml', 'w', encoding='utf-8') as f:
             f.write(str(soup))
 
 
@@ -107,3 +94,23 @@ def get_users_for_scrap(PATH_TO_DB: str, n_users=10000, ) -> list:
     for i in data:
         nicknames.append(i[0])
     return nicknames
+
+
+def load_stack_of_users_to_buffer(PATH_TO_DB: str, PATH_TO_SAVE: str, n_users=10000, pre_request=True):
+    """
+    This function load user ratings data for the users with the oldest update date
+    ====================
+    :param PATH_TO_DB:
+    :param PATH_TO_SAVE:
+    :param n_users:
+    :param pre_request:
+    :return: None
+    """
+    nicknames = get_users_for_scrap(PATH_TO_DB=PATH_TO_DB, n_users=n_users)
+    if pre_request:
+        for nickname in tqdm(nicknames):
+            get_api_user_ratings_data(nickname)
+    for nickname in tqdm(nicknames):
+        get_api_user_ratings_data(nickname, PATH_TO_SAVE=PATH_TO_SAVE)
+
+
