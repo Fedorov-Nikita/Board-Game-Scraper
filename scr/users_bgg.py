2ee6dc1ccbc37d07971fe179b35ab71685fa6a3b
Nikita-Fedorov <fedorov.nvad@gmail.com>
2023-04-27 22:57:58 +0300

    [~] decompose parse_loaded_users_info and create parse_user_collection_info

--- scr/users_bgg.py
+++ scr/users_bgg.py
@@ -274,14 +274,69 @@ def add_to_deleted(PATH_TO_DB: str,
     conn.close()
 
 
+def parse_item(PATH_TO_DB, item, user_id):
+    boardgame_id = int(item.get('objectid'))
+
+    rating = float(item.find('stats').find('rating').get('value'))
+    num_of_plays = int(item.find('numplays').text)
+    try:
+        comment = item.find('comment').text
+        comment_id = get_comment_id(PATH_TO_DB=PATH_TO_DB, comment=comment)
+    except:
+        comment_id = None
+    status = item.find('status')
+
+    # Parse status from xml
+    last_modified, own, prevowned, for_trade, want, want_to_play, \
+        want_to_buy, wishlist, preordered = parse_status(status)
+
+    rating_info = get_rating_id(PATH_TO_DB, user_id, boardgame_id,
+                                rating, last_modified)
+
+    if rating_info:
+        rating_id = int(rating_info[0][0])
+
+        # Update data into db
+        upd_rating_in_db(PATH_TO_DB, num_of_plays, comment_id, own,
+                         prevowned, for_trade, want, want_to_play,
+                         want_to_buy, wishlist, preordered, rating_id)
+    else:
+        # Add data into db
+        add_rating_into_db(PATH_TO_DB, user_id, boardgame_id, rating, num_of_plays,
+                           comment_id, own, prevowned, for_trade, want,
+                           want_to_play, want_to_buy, wishlist, preordered,
+                           last_modified)
+
+
+def parse_user_collection_info(PATH_TO_DB: str,
+                               ratings,
+                               nickname: str):
+    try:
+        total_ratings = int(ratings.find('items').get('totalitems'))
+    except:
+        message = str(ratings.find('message').text)
+        if message == 'Invalid username specified':
+            add_to_deleted(PATH_TO_DB=PATH_TO_DB, nickname=nickname)
+        continue
+
+    if total_ratings > 0:
+        items = ratings.find_all('item')
+        user_id = get_user_id(PATH_TO_DB=PATH_TO_DB, nickname=nickname)
+        for item in items:
+            parse_item(PATH_TO_DB, item, user_id)
+
+    # Save checking date into db
+    last_check = datetime.strptime(str(ratings.find('items').get('pubdate')),
+                                   '%a, %d %b %Y %H:%M:%S %z').isoformat()[:10]
+
+    upd_checking_date_in_db(PATH_TO_DB, last_check, nickname)
+
+
 def parse_loaded_users_info(PATH_TO_DB: str,
                             PATH_TO_READ: str,
                             PATH_TO_SAVE: str,
                             PATH_TO_DEL: str):
     files = os.listdir(PATH_TO_READ)
-    counter_update = 0
-    counter_add = 0
-    counter_deleted = 0
     for user_file in files:
         if user_file == '.DS_Store':
             continue
@@ -296,47 +351,13 @@ def parse_loaded_users_info(PATH_TO_DB: str,
             message = str(ratings.find('message').text)
             if message == 'Invalid username specified':
                 add_to_deleted(PATH_TO_DB=PATH_TO_DB, nickname=nickname)
-                counter_deleted += 1
             continue
 
         if total_ratings > 0:
             items = ratings.find_all('item')
             user_id = get_user_id(PATH_TO_DB=PATH_TO_DB, nickname=nickname)
             for item in items:
-                boardgame_id = int(item.get('objectid'))
-
-                rating = float(item.find('stats').find('rating').get('value'))
-                num_of_plays = int(item.find('numplays').text)
-                try:
-                    comment = item.find('comment').text
-                    comment_id = get_comment_id(PATH_TO_DB=PATH_TO_DB, comment=comment)
-                except:
-                    comment_id = None
-                status = item.find('status')
-
-                # Parse status from xml
-                last_modified, own, prevowned, for_trade, want, want_to_play, \
-                    want_to_buy, wishlist, preordered = parse_status(status)
-
-                rating_info = get_rating_id(PATH_TO_DB, user_id, boardgame_id,
-                                            rating, last_modified)
-
-                if rating_info:
-                    rating_id = int(rating_info[0][0])
-
-                    # Update data into db
-                    upd_rating_in_db(PATH_TO_DB, num_of_plays, comment_id, own,
-                                     prevowned, for_trade, want, want_to_play,
-                                     want_to_buy, wishlist, preordered, rating_id)
-                    counter_update += 1
-
-                else:
-                    # Add data into db
-                    add_rating_into_db(PATH_TO_DB, user_id, boardgame_id, rating, num_of_plays,
-                                       comment_id, own, prevowned, for_trade, want,
-                                       want_to_play, want_to_buy, wishlist, preordered,
-                                       last_modified)
-                    counter_add += 1
+                parse_item(PATH_TO_DB, item, user_id)
 
             os.replace(PATH_TO_READ + '/' + user_file,  # move file from buffer dir
                        PATH_TO_SAVE + '/' + user_file)  # to saving dir
@@ -350,5 +371,3 @@ def parse_loaded_users_info(PATH_TO_DB: str,
                                        '%a, %d %b %Y %H:%M:%S %z').isoformat()[:10]
 
         upd_checking_date_in_db(PATH_TO_DB, last_check, nickname)
-
-    print(f'Updated: {counter_update} ratings, added: {counter_add} ratings, deleted accounts {counter_deleted}.')
